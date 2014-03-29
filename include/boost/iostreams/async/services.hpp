97951d1c47c4c1ba06b266880672adac8785def2
Bjorn Reese <breese@sourceforge.net>
2014-03-29 13:59:09 +0100

    Changed indentation to comply with Boost.Iostreams style

--- include/boost/iostreams/async/services.hpp
+++ include/boost/iostreams/async/services.hpp
@@ -6,57 +6,57 @@
 
 namespace async_ostream {
 
-	// this implementation gives you the lowest latency when you write into an async_ostream::stream
-	class polling_service : public service {
-	public:
-		polling_service(boost::posix_time::time_duration polling_period = boost::posix_time::milliseconds(10), error_policy* e = NULL);
-		virtual ~polling_service();
+// this implementation gives you the lowest latency when you write into an async_ostream::stream
+class polling_service : public service {
+public:
+    polling_service(boost::posix_time::time_duration polling_period = boost::posix_time::milliseconds(10), error_policy* e = NULL);
+    virtual ~polling_service();
 
-		// 1. stopping
-		void stop();
+    // 1. stopping
+    void stop();
 
-		// 2. ensuring that it's stopped
-		void join(); // blocking
-		bool stopped() const; // non-blocking
+    // 2. ensuring that it's stopped
+    void join(); // blocking
+    bool stopped() const; // non-blocking
 
-	protected:
-		void work_available_impl() { /* do nothing and save CPU cycles: the polling thread will eventually find out that there was work available */ }
+protected:
+    void work_available_impl() { /* do nothing and save CPU cycles: the polling thread will eventually find out that there was work available */ }
 
-	private:
-		struct impl;
-		impl* impl_;
-	};
+private:
+    struct impl;
+    impl* impl_;
+};
 
 
-	// this implementation gives you the lowest power consumption while you wait for anything to be writte into an async_ostream::stream
-	class waiting_service : public service {
-	public:
-		waiting_service(error_policy* e = NULL);
-		virtual ~waiting_service();
+// this implementation gives you the lowest power consumption while you wait for anything to be writte into an async_ostream::stream
+class waiting_service : public service {
+public:
+    waiting_service(error_policy* e = NULL);
+    virtual ~waiting_service();
 
-		// 1. stopping
-		void stop();
+    // 1. stopping
+    void stop();
 
-		// 2. ensuring that it's stopped
-		void join(); // blocking
-		bool stopped() const; // non-blocking
+    // 2. ensuring that it's stopped
+    void join(); // blocking
+    bool stopped() const; // non-blocking
 
-	protected:
-		void work_available_impl();
+protected:
+    void work_available_impl();
 
-	private:
-		struct impl;
-		impl* impl_;
-	};
+private:
+    struct impl;
+    impl* impl_;
+};
 
-	// this implementation is mainly useful for debugging, 
-	// because all the writing and reading will be done deterministically on one thread (inside ~stream destructor)
-	class idle_service : public service {
-	public:
-		idle_service() : service(false, NULL) {}
-	protected:
-		void work_available_impl() {}
-	};
+// this implementation is mainly useful for debugging, 
+// because all the writing and reading will be done deterministically on one thread (inside ~stream destructor)
+class idle_service : public service {
+public:
+    idle_service() : service(false, NULL) {}
+protected:
+    void work_available_impl() {}
+};
 
 }
 
--- src/async/asio_service_wrappers.cpp
+++ src/async/asio_service_wrappers.cpp
@@ -2,148 +2,148 @@
 
 namespace async_ostream {
 
-	// if we are supposed to ensure no further work is scheduled
-	struct asio_service_wrapper::set_stopped {
-		void operator() () { 
-			assert(!target_->queues_empty_ && "this means that queues_empty_ variable is lying");
-			target_->stopped_ = 1;
-		}
-		asio_service_wrapper* target_;
-		set_stopped(asio_service_wrapper* t) : target_(t) {}
-	};
-	// if we are supposed to prove that no more scheduled work in the queue
-	struct asio_service_wrapper::set_queues_empty {
-		void operator() () { 
-			assert(!target_->queues_empty_ && "this means that queues_empty_ variable is lying");
-			target_->queues_empty_ = 1;
-		}
-		asio_service_wrapper* target_;
-		set_queues_empty(asio_service_wrapper* t) : target_(t) {}
-	};
-	asio_service_wrapper::~asio_service_wrapper() {
-		// ensure that no new background work is being enqueued anynmore
-		s_.post(set_stopped(this));
-		while (!stopped_) {
-			boost::detail::yield((unsigned)-1); // don't burn CPU cycles
-			if (s_.stopped())
-				break;
-		}
-		// wait until there is no more background work left in the queue
-		s_.post(set_queues_empty(this));
-		while (!queues_empty_) {
-			boost::detail::yield((unsigned)-1); // don't burn CPU cycles
-			if (s_.stopped())
-				break;
-		}
-		// good! now we can proceed to destructing everything associated with this service wrapper
-	}
-	asio_service_wrapper::asio_service_wrapper(bool suppress_work_available_call, asio_service& s, error_policy* ep) : 
-		service(suppress_work_available_call, ep),
-		s_(s), keep_asio_service_running_(s), 
-		stopped_(0), queues_empty_(0)
-	{}
+// if we are supposed to ensure no further work is scheduled
+struct asio_service_wrapper::set_stopped {
+    void operator() () { 
+        assert(!target_->queues_empty_ && "this means that queues_empty_ variable is lying");
+        target_->stopped_ = 1;
+    }
+    asio_service_wrapper* target_;
+    set_stopped(asio_service_wrapper* t) : target_(t) {}
+};
+// if we are supposed to prove that no more scheduled work in the queue
+struct asio_service_wrapper::set_queues_empty {
+    void operator() () { 
+        assert(!target_->queues_empty_ && "this means that queues_empty_ variable is lying");
+        target_->queues_empty_ = 1;
+    }
+    asio_service_wrapper* target_;
+    set_queues_empty(asio_service_wrapper* t) : target_(t) {}
+};
+asio_service_wrapper::~asio_service_wrapper() {
+    // ensure that no new background work is being enqueued anynmore
+    s_.post(set_stopped(this));
+    while (!stopped_) {
+        boost::detail::yield((unsigned)-1); // don't burn CPU cycles
+        if (s_.stopped())
+            break;
+    }
+    // wait until there is no more background work left in the queue
+    s_.post(set_queues_empty(this));
+    while (!queues_empty_) {
+        boost::detail::yield((unsigned)-1); // don't burn CPU cycles
+        if (s_.stopped())
+            break;
+    }
+    // good! now we can proceed to destructing everything associated with this service wrapper
+}
+asio_service_wrapper::asio_service_wrapper(bool suppress_work_available_call, asio_service& s, error_policy* ep) : 
+    service(suppress_work_available_call, ep),
+    s_(s), keep_asio_service_running_(s), 
+    stopped_(0), queues_empty_(0)
+{}
 
-	struct asio_service_wrapper_low_enqueue_latency::drain_all_buffers_and_poll {
-		void operator() (const boost::system::error_code&) { 
-			assert(!target_->queues_empty_ && "this means that queues_empty_ variable is lying");
-			if (target_->stopped_)
-				return;
-			// drain the buffers until there is no more data
-			for (;;) {
-				if (!target_->run())
-					break;
-			}
-			// run again when time comes
-			target_->poll_timer_->expires_at(target_->poll_timer_->expires_at() + target_->poll_interval_);
-			target_->poll_timer_->async_wait(drain_all_buffers_and_poll(target_));
-		}
-		asio_service_wrapper_low_enqueue_latency* target_;
-		drain_all_buffers_and_poll(asio_service_wrapper_low_enqueue_latency* t) : target_(t) {}
-	};
+struct asio_service_wrapper_low_enqueue_latency::drain_all_buffers_and_poll {
+    void operator() (const boost::system::error_code&) { 
+        assert(!target_->queues_empty_ && "this means that queues_empty_ variable is lying");
+        if (target_->stopped_)
+            return;
+        // drain the buffers until there is no more data
+        for (;;) {
+            if (!target_->run())
+                break;
+        }
+        // run again when time comes
+        target_->poll_timer_->expires_at(target_->poll_timer_->expires_at() + target_->poll_interval_);
+        target_->poll_timer_->async_wait(drain_all_buffers_and_poll(target_));
+    }
+    asio_service_wrapper_low_enqueue_latency* target_;
+    drain_all_buffers_and_poll(asio_service_wrapper_low_enqueue_latency* t) : target_(t) {}
+};
 
-	asio_service_wrapper_low_enqueue_latency::asio_service_wrapper_low_enqueue_latency(asio_service& s, error_policy* ep, boost::posix_time::time_duration poll_interval) : 
-		asio_service_wrapper(true /* don't call work_avaiable() */, s, ep), poll_interval_(poll_interval)
-	{
-		poll_timer_.reset(new boost::asio::deadline_timer(s, poll_interval));
-		poll_timer_->async_wait(drain_all_buffers_and_poll(this));
-	}
-	asio_service_wrapper_low_enqueue_latency::~asio_service_wrapper_low_enqueue_latency() {
-		// ensure that no new background work is being enqueued anynmore
-		s_.post(set_stopped(this));
-		while (!stopped_) {
-			boost::detail::yield((unsigned)-1); // don't burn CPU cycles
-			if (s_.stopped())
-				break;
-		}
-		// good! now we can proceed to destructing everything associated with this service wrapper
-		if (poll_timer_.get())
-			poll_timer_->cancel();
-	}
+asio_service_wrapper_low_enqueue_latency::asio_service_wrapper_low_enqueue_latency(asio_service& s, error_policy* ep, boost::posix_time::time_duration poll_interval) : 
+    asio_service_wrapper(true /* don't call work_avaiable() */, s, ep), poll_interval_(poll_interval)
+{
+    poll_timer_.reset(new boost::asio::deadline_timer(s, poll_interval));
+    poll_timer_->async_wait(drain_all_buffers_and_poll(this));
+}
+asio_service_wrapper_low_enqueue_latency::~asio_service_wrapper_low_enqueue_latency() {
+    // ensure that no new background work is being enqueued anynmore
+    s_.post(set_stopped(this));
+    while (!stopped_) {
+        boost::detail::yield((unsigned)-1); // don't burn CPU cycles
+        if (s_.stopped())
+            break;
+    }
+    // good! now we can proceed to destructing everything associated with this service wrapper
+    if (poll_timer_.get())
+        poll_timer_->cancel();
+}
 
-	// what's done on the background thread
-	struct asio_service_wrapper_low_overall_latency::drain_all_buffers_and_spin {
-		void operator() () { 
-			assert(!target_->queues_empty_ && "this means that queues_empty_ variable is lying");
-			if (target_->stopped_)
-				return;
-			// drain the buffers until there is no more data
-			for (;;) {
-				if (!target_->run())
-					break;
-			}
-			// schedule to be called again ASAP *if* we are supposed to spin
-			target_->s_.post(drain_all_buffers_and_spin(target_));
-		}
-		asio_service_wrapper_low_overall_latency* target_;
-		drain_all_buffers_and_spin(asio_service_wrapper_low_overall_latency* t) : target_(t) {}
-	};
-	// what's done on the foreground
-	asio_service_wrapper_low_overall_latency::asio_service_wrapper_low_overall_latency(asio_service& s, error_policy* ep) : 
-		asio_service_wrapper(true /* don't call work_avaiable() */, s, ep)
-	{
-		s_.post(drain_all_buffers_and_spin(this));
-	}
+// what's done on the background thread
+struct asio_service_wrapper_low_overall_latency::drain_all_buffers_and_spin {
+    void operator() () { 
+        assert(!target_->queues_empty_ && "this means that queues_empty_ variable is lying");
+        if (target_->stopped_)
+            return;
+        // drain the buffers until there is no more data
+        for (;;) {
+            if (!target_->run())
+                break;
+        }
+        // schedule to be called again ASAP *if* we are supposed to spin
+        target_->s_.post(drain_all_buffers_and_spin(target_));
+    }
+    asio_service_wrapper_low_overall_latency* target_;
+    drain_all_buffers_and_spin(asio_service_wrapper_low_overall_latency* t) : target_(t) {}
+};
+// what's done on the foreground
+asio_service_wrapper_low_overall_latency::asio_service_wrapper_low_overall_latency(asio_service& s, error_policy* ep) : 
+    asio_service_wrapper(true /* don't call work_avaiable() */, s, ep)
+{
+    s_.post(drain_all_buffers_and_spin(this));
+}
 
 
-	struct asio_service_wrapper_low_power::drain_all_buffers_once {
-		void operator() () { 
-			assert(!target_->queues_empty_ && "this means that queues_empty_ variable is lying");
-			if (target_->stopped_)
-				return;
-			for (;;) {
-				// 1. drain the buffers until there is no more data
-				for (;;) {
-					if (!target_->run())
-						break;
-				}
+struct asio_service_wrapper_low_power::drain_all_buffers_once {
+    void operator() () { 
+        assert(!target_->queues_empty_ && "this means that queues_empty_ variable is lying");
+        if (target_->stopped_)
+            return;
+        for (;;) {
+            // 1. drain the buffers until there is no more data
+            for (;;) {
+                if (!target_->run())
+                    break;
+            }
 
-				// 2. set the flag that we are not draining anymore
-				target_->draining_.store(0, boost::memory_order_release);
+            // 2. set the flag that we are not draining anymore
+            target_->draining_.store(0, boost::memory_order_release);
 
-				// 3. ensure that queues are indeed still empty after we indicated that we aren't draining anymore
-				if (!target_->run())
-					break;
+            // 3. ensure that queues are indeed still empty after we indicated that we aren't draining anymore
+            if (!target_->run())
+                break;
 
-				// 4. but if they weren't empty again, we have to redo the whole thing
-				target_->draining_.store(1, boost::memory_order_release);
-			}
-		}
-		asio_service_wrapper_low_power* target_;
-		drain_all_buffers_once(asio_service_wrapper_low_power* t) : target_(t) {}
-	};
+            // 4. but if they weren't empty again, we have to redo the whole thing
+            target_->draining_.store(1, boost::memory_order_release);
+        }
+    }
+    asio_service_wrapper_low_power* target_;
+    drain_all_buffers_once(asio_service_wrapper_low_power* t) : target_(t) {}
+};
 
-	// what's done on the foreground thread
-	asio_service_wrapper_low_power::asio_service_wrapper_low_power(asio_service& s, error_policy* ep) : 
-		asio_service_wrapper(false /* call work_avaiable() */, s, ep),
-		draining_(0)
-	{}
-	void asio_service_wrapper_low_power::work_available_impl() {
-		// 1. check that nobody is draining those queues already
-		size_t draining = draining_.load(boost::memory_order_acquire);
-		// 2. if nobody is draining the queues now, schedule that to happen
-		if (!draining) {
-			draining_.store(1, boost::memory_order_release);
-			s_.post(drain_all_buffers_once(this));
-		}
-	}
+// what's done on the foreground thread
+asio_service_wrapper_low_power::asio_service_wrapper_low_power(asio_service& s, error_policy* ep) : 
+    asio_service_wrapper(false /* call work_avaiable() */, s, ep),
+    draining_(0)
+{}
+void asio_service_wrapper_low_power::work_available_impl() {
+    // 1. check that nobody is draining those queues already
+    size_t draining = draining_.load(boost::memory_order_acquire);
+    // 2. if nobody is draining the queues now, schedule that to happen
+    if (!draining) {
+        draining_.store(1, boost::memory_order_release);
+        s_.post(drain_all_buffers_once(this));
+    }
+}
 }
